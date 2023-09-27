9a0d1805198cd529024099bc68fd9eec85f598e0
franciscomuniz <91153874+franciscomuniz@users.noreply.github.com>
2023-09-27 17:49:00 -0600

    Java file being read added

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 '''Python program to "parse" java files'''
 # Assumption: Indentation is already correct
+def PPL_Projecr02_main():
+    with open('test.java') as f:
+        print(f.read())
+
+if __name__ == '__main__':
+    PPL_Projecr02_main()
