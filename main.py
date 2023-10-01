1b7afc5152e0f7adb1bcb27406a80234bac7008d
daskimmel1 <david.soth-kimmel@everstream.ai>
2023-10-01 14:01:32 -0600

    Cleanup

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ import re
 
 # Define a regular expression pattern to match Java method declarations
 method_pattern = re.compile(r'\s*(public|private|protected|static)?\s*[\w<>,]+\s+(\w+)\s*\([^)]*\)\s*{')
+if_pattern = re.compile(r'^\s*if\s*\(.+\)\s*\{?$')
+else_if_pattern = re.compile(r'^\s*else\s*if\s*\(.+\)\s*\{?$')
+else_pattern = re.compile(r'^\s*else\s*\{?$')
+
 
 def is_decision_structure(line):
     # Remove leading and trailing whitespaces
     line = line.strip()
 
     # Regular expressions to match common decision structures
-    if_pattern = re.compile(r'^\s*if\s*\(.+\)\s*\{?$')
-    else_if_pattern = re.compile(r'^\s*else\s*if\s*\(.+\)\s*\{?$')
-    else_pattern = re.compile(r'^\s*else\s*\{?$')
-
     # Check if the line matches any of the patterns
     if if_pattern.match(line) or else_if_pattern.match(line) or else_pattern.match(line):
         return True
 
     return False
+
 
 # Function to check if a line contains a method declaration
 def is_method_declaration(line):
@@ -39,7 +40,7 @@ class JavaParser:
         if self.method_missing_starting_brace and not stripped_line.startswith("{"):
 
             self.method_missing_starting_brace = False
-            line = "// MISSING '{' BRACE WAS ADDED HERE \n" + (" " * self.method_indent_count) + "{\n" + line
+            line = (" " * self.method_indent_count) + "// MISSING '{' BRACE WAS ADDED HERE \n" + (" " * self.method_indent_count) + "{\n" + line
 
         if self.inside_method and stripped_line.startswith("}"):
             self.inside_method = False
